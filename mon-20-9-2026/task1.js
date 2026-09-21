1212c7db4e11b698f5fb16ae80fb574bda6971a9
talaaloqaili-arch <tala.aloqaili@gmail.com>
2026-09-21 15:45:12 +0300

    Initial commit

---
+++ mon-20-9-2026/task1.js
@@ -0,0 +1,29 @@
+'use strict'
+console.log("Data type of 100=",typeof(100));
+console.log("Data type of 73.9=",typeof(73.9));
+console.log("Data type of NaN=",typeof(NaN));
+console.log("Data type of water=",typeof("Water"));
+console.log("Data type of water=",(typeof(false)));
+console.log("Data type of 100=",typeof((9 != 11)));
+console.log("Data type of 100=",typeof(Infinity));
+console.log(typeof("Orang" + "e"));
+console.log(typeof("Orange" - "s"));
+console.log("Orange" - "s");
+console.log("4" + "8");
+console.log("4" - "8");
+console.log("name" + 3);
+console.log("name" - 3);
+console.log(82 * "word");
+console.log(1 + "hello");
+console.log("hello" + 1);
+console.log(1 + true);
+console.log("hello" + true);
+console.log(typeof (Infinity));
+console.log(1 == '1');
+console.log(1 === '1');
+
+
+
+
+
+
